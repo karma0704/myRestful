a731f8871cdab0e26d71edbbe3e807671745a6de
karma0704 <kkarma@126.com>
2020-12-01 10:52:23 +0800

    'modify'

--- myRestApi/rest_app01/router/urls.py
+++ myRestApi/rest_app01/router/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('v1/schools', school_v1.SchoolListView.as_view(), name='schools_v1'),
     path('v1/schools/<int:pk>', school_v2.SchoolDetailView.as_view(), name='schoolDetail_v1'),
     # rest接口v1.2.0版本url设置
-    path('v2/schools', school_v1.SchoolListView.as_view(), name='schools_v2'),
+    path('v2/schools', school_v2.SchoolListView.as_view(), name='schools_v2'),
     path('v2/schools/<int:pk>', school_v2.SchoolDetailView.as_view(), name='schoolDetail_v2'),
     # rest接口v2.0.0版本url设置
     path('v3/schools', school_v3.SchoolViewSet.as_view({
